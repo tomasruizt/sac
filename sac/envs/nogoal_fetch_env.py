88ece11e310d8c23e82d2dc169dfea62b08cee7e
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2020-10-07 17:32:13 +0200

    Add FetchPickAndPlace

--- sac/envs/nogoal_fetch_env.py
+++ sac/envs/nogoal_fetch_env.py
@@ -1,13 +1,15 @@
+from typing import Union
+
 import gym
 import time
 
 import numpy as np
-from gym.envs.robotics import FetchReachEnv
+from gym.envs.robotics import FetchReachEnv, FetchPickAndPlaceEnv
 from gym.wrappers import TimeLimit, FilterObservation, FlattenObservation
 
 
-def make_nogoal_fetchreach_env():
-    env = FilterObservation(env=FetchReachEnv(), filter_keys=["observation", "achieved_goal"])
+def wrap_fetch_env(fetch_env: Union[FetchReachEnv, FetchPickAndPlaceEnv]):
+    env = FilterObservation(env=fetch_env, filter_keys=["observation", "achieved_goal"])
     bounded_space = gym.spaces.Box(low=np.array([1.2, 0, 0.4]), high=np.array([1.6, 1.2, 0.9]))
     env.observation_space = gym.spaces.Dict(spaces=dict(
         achieved_goal=bounded_space,
@@ -16,8 +18,16 @@ def make_nogoal_fetchreach_env():
     return TimeLimit(env=FlattenObservation(env), max_episode_steps=1000)
 
 
+def make_nogoal_fetchreach_env():
+    return wrap_fetch_env(FetchReachEnv())
+
+
+def make_nogoal_fetchpickandplace_env():
+    return wrap_fetch_env(FetchPickAndPlaceEnv())
+
+
 if __name__ == '__main__':
-    env = make_nogoal_fetchreach_env()
+    env = make_nogoal_fetchpickandplace_env()
     assert isinstance(env.reset(), np.ndarray)
     while True:
         env.step(env.action_space.sample())
